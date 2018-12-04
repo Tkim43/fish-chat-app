c931c95e75677587f5b77f3b012f0d133fe53f57
Tiffany Kim <minikim9@hotmail.com>
2018-12-04 13:35:49 -0800

    completed size nav bar and 404 page

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props =>{
+    return(
+        <div className="center">
+        <h1>Want to talk to fish?</h1>
+        <p className="grey-text">The Fish Chat Application</p>
+    </div>
+    )
+}
