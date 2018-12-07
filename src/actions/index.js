ec02d1599a4fbfbae88fe6edeb64f89dc38d76c7
Tiffany Kim <minikim9@hotmail.com>
2018-12-07 11:46:47 -0800

    added firebase functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
 import types from './types'
+import {db} from '../firebase';
+
+// export const getAllMessages = () => async dispatch =>{
+
+// }
+
+export function getAllMessages() {
+    return async function (dispatch){
+        db.ref('/messages').on('value', (snapshot) => {
+            dispatch({
+                type: types.GET_CHAT_MESSAGES,
+                messages: snapshot.val()
+            });
+        });
+    }
+}
---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+
+export default {
+    apiKey: "AIzaSyBQdwuUJJsBVR7-o2cLuElnJuGfcYSh8_o",
+    authDomain: "fish-chat-85700.firebaseapp.com",
+    databaseURL: "https://fish-chat-85700.firebaseio.com",
+    projectId: "fish-chat-85700",
+    storageBucket: "fish-chat-85700.appspot.com",
+    messagingSenderId: "747478314370"
+  };
